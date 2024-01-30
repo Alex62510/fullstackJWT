fb9f05ecc4c5cceac720e1956309db2072bd73a6
Alex62510 <eagle625@tut.by>
2024-01-30 14:05:18 +0300

    fix index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser'
-import path from 'path'
+import path from "path"
 
 dotenv.config()
 mongoose.connect(process.env.MONGO).then(() => {
@@ -17,11 +17,6 @@ const __dirname=path.resolve()
 
 const app = express()
 
-app.use(express.static(path.join(__dirname,'/client/dist')))
-app.get('*',(req,res)=>{
-res.sendFile(path.join(__dirname,'client','dist','index.html'))
-})
-
 app.use(express.json())
 app.use(cookieParser())
 app.listen(3000, () => {
@@ -30,8 +25,13 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes)
 app.use("/api/auth", authRoutes)
 
+app.use(express.static(path.join(__dirname,'/client/dist')))
 
-app.use((err, req, res, next) => {
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
+app.use((err, req, res) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
     return res.status(statusCode).json({
