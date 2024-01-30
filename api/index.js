a9fc49dfc6efaa155a266067c952db5d19ecbadb
Alex62510 <eagle625@tut.by>
2024-01-30 10:41:08 +0300

    add server

---
+++ api/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
+import path from 'path'
+
+dotenv.config()
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('connected to MDB')
+}).catch((e) => {
+    console.log(e)
+})
+
+const __dirname=path.resolve()
+
+const app = express()
+
+app.use(express.static(path.join(__dirname,'/JWT_Project/dist')))
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,'JWT_Project','dist','index.html'))
+})
+
+app.use(express.json())
+app.use(cookieParser())
+app.listen(3000, () => {
+    console.log('server run on port 3000')
+})
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
---
+++ api/model/user.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    profilePicture: {
+        type: String,
+        default: "https://t4.ftcdn.net/jpg/02/29/75/83/240_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg"
+    }
+}, {timestamps: true})
+const User = mongoose.model('User', userSchema)
+export default User
